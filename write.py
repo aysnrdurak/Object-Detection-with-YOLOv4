7f068a863a69b09a9240a96b6f755bbe771d1d85
Ayşenur Durak <64005350+aysnrdurak@users.noreply.github.com>
2022-05-06 12:35:36 +0300

    Delete write.py

--- write.py
+++
@@ -1,7 +0,0 @@
-import os
-
-with open("output.txt", "w") as a:
-    for path, subdirs, files in os.walk(r'/home/aysenur/Desktop/data'):
-       for filename in files:
-         f = os.path.join(path, filename)
-         a.write(str(f) + os.linesep)
